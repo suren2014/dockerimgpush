8c758c38e7ab237369a111da57a409de3910e1f6
suren2014 <suren1325@gmail.com>
2026-02-13 20:50:45 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+def normal_function():
+  return "This will be deployed on the Agent server"
+
+@app.route("/")
+def home():
+  return normal_function()
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000)
