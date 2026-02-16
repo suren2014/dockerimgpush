fc10f800c17a9480d0e8aa7a1bd0c8cf650595ee
suren2014 <suren1325@gmail.com>
2026-02-16 21:12:08 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -3,11 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 def normal_function():
-  return "This will be deployed on the Agent server"
+    return "This will be deployed on the Agent server"
 
 @app.route("/")
 def home():
-  return normal_function()
+    return normal_function()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
